e9a0ac9d92324f9812eab83bbe8efe2d70faffd2
alawammo <alawammo@oregonstate.edu>
2019-01-28 10:58:56 -0800

    fixed minor typo

--- src/test/integration/integration_tests.py
+++ src/test/integration/integration_tests.py
@@ -63,7 +63,7 @@ def test_missing_param(self, param, valid_params):
 def set_course_data(subject):
     terms = utils.get_courses(None)
     # Nested for loop to loop through each term, and each course, to return
-    # the first valid term, course, and a random section ther is
+    # the first valid term, course, and a random section
     for term in terms.json():
         term_id = term["id"]
         (valid_year, valid_term) = term_id.split("-")
@@ -71,11 +71,11 @@ def set_course_data(subject):
         # looping through each course in the term.
         for course in courses.json():
             valid_course = course["id"]
-            # if sections is empty loop agian to find a course
+            # if sections is empty, then loop again to find a course
             # with a valid section
             if course["sections"]:
                 # include a random section in the response
-                random_section = random.randint(0, len(course["sections"]) - 1)
+                random_section = random.sample(course["sections"], 1)
                 valid_section = course["sections"][random_section]["name"]
                 global book_params
                 book_params = {
@@ -89,7 +89,7 @@ def set_course_data(subject):
                 # exit the function with return. Otherwise loop again
                 return
 
-    # if this line was excuted that means there was nothing found in the loop.
+    # if this line was executed that means there was nothing found in the loop.
     exit("No valid courses were found for this search")
 
 
